78a108e98affa138bf3c80d96fe295ea71175395
samir parab <samirparab.c@gmail.com>
2019-11-27 10:51:58 -0700

    getAppointmentsForDay and getInterview functions added and respective tests passed.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -13,3 +13,4 @@ export default function DayList(props) {
     ));
     return days;
 };
+
---
+++ src/helpers/selectors.js
@@ -0,0 +1,33 @@
+// Returns the Appointments for a given day (ex: Monday)
+export function getAppointmentsForDay(state, day) {
+  let result = [];
+  const filteredDays = state.days.filter(itemId => itemId.name === day);
+
+  if (filteredDays.length === 0) {
+    return filteredDays;
+  }
+
+  filteredDays[0].appointments.forEach(itemId => {
+    result.push(state.appointments[itemId]);
+  });
+
+  return result;
+}
+
+const getInterview = (state, interview) => {
+  if (interview) {
+    let result = { student: interview.student };
+    // console.log("heremmmmmmmmmmmmmmmmmmmmmmmm", state.interviewers)
+    for (const key in state.interviewers) {
+      if (interview.interviewer === Number(key)) {
+        //console.log("aaaaaaaaaaaa", state.interviewers[key])
+        result = { ...result, interviewer: {...state.interviewers[key]} };
+      }
+    }
+    // console.log("bbbbbbbbbbb", result)
+    return result;
+  }
+  return null;
+};
+
+module.exports.getInterview = getInterview;
